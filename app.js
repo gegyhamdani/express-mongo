47577386c760a5647ea867a129b6c698f05102ea
gegyhamdani <gegyhamdani@gmail.com>
2020-04-07 19:48:15 +0700

    add urlencoded and json middleware

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv/config");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const postsRoute = require("./routes/posts");
 
 app.use("/posts", postsRoute);
+
 
 app.get("/", (req, res) => {
   res.send("Success");
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
 
 router.get("/", (req, res) => {
   res.send("Routing Test");
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
 });
 
 module.exports = router;
